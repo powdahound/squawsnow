fc4c7cd0c96028299cf9936649c3bfbcc1d527f7
Garret Heaton <powdahound@gmail.com>
2015-11-07 11:50:50 -0800

    Allow startup even if mail is not configured

--- libs/mail.js
+++ libs/mail.js
@@ -1,15 +1,25 @@
-// the heroku addon sets MAILGUN_SMTP_LOGIN, so just pull the domain from there
-var domain = process.env.MAILGUN_SMTP_LOGIN.split('@')[1];
-var target = process.env.EMAIL_TARGET;
+var mailgun = null;
 
-var mailgun = require('mailgun-js')({
-  apiKey: process.env.MAILGUN_API_KEY,
-  domain: domain
-});
+try {
+  // the heroku addon sets MAILGUN_SMTP_LOGIN, so just pull the domain from there
+  var domain = process.env.MAILGUN_SMTP_LOGIN.split('@')[1];
+  var target = process.env.EMAIL_TARGET;
+
+  mailgun = require('mailgun-js')({
+    apiKey: process.env.MAILGUN_API_KEY,
+    domain: domain
+  });
+} catch (e) {
+  console.error("Unable to configure mail: ", e);
+}
 
 // todo: send email to a list people can subscribe to?
 
 function notify(data) {
+  if (mailgun === null) {
+    console.error('Mail not configured, so can not send.');
+    return;
+  }
   var subject = "New snow on " + data['date'] + ": "
     + data['6200-new'] + " at 6200', " + data['8000-new'] + " at 8000'";
 
